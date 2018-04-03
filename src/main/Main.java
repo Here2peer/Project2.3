3ad2a2958f0fb9231ba1245adba7b01d9995ebd2
Roobin <roobin1999@gmail.com>
2018-04-03 15:40:28 +0200

    Logischere Main

--- src/main/Main.java
+++ src/main/Main.java
@@ -11,32 +11,32 @@ import java.awt.Color;
 import javax.swing.JFrame;
 
 public class Main{
-	
+
 	private Model model;
 	private JFrame mainFrame;
 	private AbstractView gridView;
 	private AbstractController controller;
-	
+
 	public Main() {
 		model = new Model();
 		GridView gridview = new GridView(model);
 		controller = new Controller(model);
 
 		mainFrame=new JFrame("Reversi - ITV2G, Groep 2");
-		mainFrame.setSize(800, 600);
+		mainFrame.setSize(900, 800);
 		mainFrame.setResizable(true);
 		mainFrame.setLayout(null);
 		mainFrame.getContentPane().add(gridview);
-        mainFrame.getContentPane().add(controller);
+		mainFrame.getContentPane().add(controller);
 		mainFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		mainFrame.setVisible(true);
 		mainFrame.getContentPane().setBackground(Color.white);
 
-		gridview.setBounds(10,00,550,550);
+		gridview.setBounds(80,80,550,550);
 		gridview.setBackground(Color.WHITE);
 		gridview.updateView();
 
-		controller.setBounds(570,0,200,550);
+		controller.setBounds(740,80,200,550);
 		controller.setBackground(Color.WHITE);
 		TicTacToe game = new TicTacToe(gridview.getGridView());
 	}
