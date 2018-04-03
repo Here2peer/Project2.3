85d7a3f7a894037635985aeda76d6e9f7b11762e
Roobin <roobin1999@gmail.com>
2018-04-03 13:39:02 +0200

    Kleine aanpassingen

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,6 +4,7 @@ import model.Model;
 import view.AbstractView;
 import view.GridView;
 import controller.*;
+import view.TictactoeView;
 
 import java.awt.Color;
 
@@ -13,27 +14,43 @@ public class Main{
 	
 	private Model model;
 	private JFrame mainFrame;
-	private AbstractView gridView;
+	private AbstractView gameView;
+	private AbstractView tictactoeView;
 	private AbstractController controller;
-	
+
+	private boolean mode = true;
+
+	/*
+
+	Boolean mode -> true -> Reversi
+					false -> Tictactoe
+
+	*/
+
 	public Main() {
 		model = new Model();
-		gridView = new GridView(model);
+
+		if(mode) {
+			gameView = new GridView(model);
+		} else {
+			gameView = new TictactoeView(model);
+		}
+
 		controller = new Controller(model);
 
 		mainFrame=new JFrame("Reversi - ITV2G, Groep 2");
 		mainFrame.setSize(800, 600);
 		mainFrame.setResizable(true);
 		mainFrame.setLayout(null);
-		mainFrame.getContentPane().add(gridView);
+		mainFrame.getContentPane().add(gameView);
         mainFrame.getContentPane().add(controller);
 		mainFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		mainFrame.setVisible(true);
 		mainFrame.getContentPane().setBackground(Color.white);
 
-		gridView.setBounds(10,00,550,550);
-		gridView.setBackground(Color.WHITE);
-		gridView.updateView();
+		gameView.setBounds(10,00,550,550);
+		gameView.setBackground(Color.WHITE);
+		gameView.updateView();
 
 		controller.setBounds(570,0,200,550);
 		controller.setBackground(Color.WHITE);
---
+++ src/view/TictactoeView.java
@@ -0,0 +1,12 @@
+package view;
+
+import model.Model;
+
+import javax.swing.*;
+
+public class TictactoeView extends AbstractView {
+
+    public TictactoeView(Model model){
+        super(model);
+    }
+}
